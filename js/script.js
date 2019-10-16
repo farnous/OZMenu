6c13bf116a1344d1640d8d59f45d27710f34a620
farnous <ffarnous@gmail.com>
2019-10-15 22:53:15 -0400

    show category on homepage

--- js/script.js
+++ js/script.js
@@ -10,16 +10,30 @@
 */
 //Data:
 const categories = [
-    {url : "", title : "0", image : ""},
-    {url : "", title : "1", image : ""},
-    {url : "", title : "2", image : ""},
-    {url : "", title : "3", image : ""},
-    {url : "", title : "4", image : ""}
+    {url : "soup", title : "Soup", image : "images/category/soup.png"},
+    {url : "starter", title : "Starters", image : "images/category/starters.png"},
+    {url : "leaf", title : "LEAF LOVERS", image : "images/category/leaf.png"},
+    {url : "sushi", title : "FROM THE SUSHI CORNER", image : "images/category/sushi.png"},
+    {url : "pasta", title : "PASTAS", image : "images/category/pasta.png"},
+    {url : "pizza", title : "PIZZA", image : "images/category/pizza.png"},
+    {url : "risotto", title : "RISOTTO", image : "images/category/risotto.png"},
+    {url : "burger", title : "SANDWICHES & BURGERS", image : "images/category/burger.png"},
+    {url : "sliders", title : "SLIDER'S", image : "images/category/sliders.png"},
+    {url : "signature", title : "OUR SIGNATURE", image : "images/category/signature.png"},
+    {url : "steak-seafood", title : "Fire-Grilled Steaks & Sea Food", image : "images/category/steak.png"},
+    {url : "dessert", title : "FELZY DESSERT", image : "images/category/dessert.png"},
 ];
 
 const dishes = [
-    {title : "", price : "", image : "", description : ""},
-    {title : "", price : "", image : "", description : ""},
+    {
+      title : "Ginger Pumpkin Bisque",
+      category : "soup", price : "", image : "",
+      description : "Butternut Squash with Pumpkin Seed and Sage, Caramelized Ginger "
+    },
+    {title : "Wild Mushroom Cappuccino", category : "soup", price : "", image : "", description : "Mix of Wild Mushroom Toped with White Truffle Foam "},
+    {title : "Grand Mama", category : "soup", price : "", image : "", description : "Homemade Chicken Soup Recipe "},
+    {title : "Tom Yum", category : "soup", price : "", image : "", description : "Thai Hot & Sour, Sea Bass, Prawns, Vermicelli, Mushroom Wine Tomato"},
+    {title : "Gyoza ", category : "", price : "", image : "", description : "Lightly Pan Fried or Steamed Chicken Dumplings with Shanghai Sauce"},
 ]
 
 
@@ -36,37 +50,50 @@ function getQueryVariable(variable)
        return(false);
 }
 
-
+//create box
+function createBox(content, css = false) {
+  let box = document.createElement('li');
+  document.querySelector('#category ul').appendChild(box);
+  box.innerHTML = content;
+  if(css){
+    box.setAttribute('style', css);
+  }
+}
+//add space after boxes
+function addSpace() {
+  let sp = document.createElement('div');
+  sp.classList.add("sp");
+  document.querySelector('#category ul').appendChild(sp);
+}
 
 //show category
 function showCategory(){
     categories.forEach(cat => {
-        let category = document.createElement('li');
-        document.querySelector('#category ul').appendChild(category);
-        console.log(cat);
+        createBox(`<a href="category.html?m=${cat.url}"><h2>${cat.title}</h2></a>`, `background: url(${cat.image}) 50% 50% no-repeat; background-size: cover`);
     });
-    let sp = document.createElement('div');
-    sp.classList.add("sp");
-    document.querySelector('#category ul').appendChild(sp);
+    addSpace()
 }
 
-function openCategory(index){
-    //create an image element with class and set the src attr
-    console.log(categories[index]);
-}
+// function openCategory(index){
+//     //create an image element with class and set the src attr
+//     console.log(categories[index]);
+// }
 
 //show category
-function showDishes(){
-    dishes.forEach(cat => {
-        console.log(cat);
+function showDishes(cat){
+    let cdished = dishes.filter ( dish => dish.category === cat)
+    cdished.forEach(dish => {
+        createBox(`${dish.title}`)
+        console.log(dish);
     });
+    addSpace();
 }
 
 function openDish(index) {
     console.log(dishes[index]);
 }
 //index.html
-showCategory();
+// showCategory();
 
 //category.html?cat=3
 //openCategory(getQueryVariable('cat'));
